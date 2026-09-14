a6f5adf9c1a8a97a42f737bed0e9d63b490bc0b2
Max <kvyatkovskymaxim7@gmail.com>
2026-09-14 15:04:11 +0300

    feat: add Service entity

---
+++ NailArt/Domain/Service.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain
+{
+    public class Service
+    {
+        public Ulid Id { get; private set; }
+        public string Name { get; set; }
+        public string? ShortDescription { get; set; }
+        public int DurationHours { get; set; }
+        public int DurationMinutes { get; set; }
+        public decimal Price { get; set; }
+        public Service(string name, string? shortDescription, int durationHours, int durationMinutes, decimal price)
+        {
+            Id = Ulid.NewUlid();
+            Name = name;
+            ShortDescription = shortDescription;
+            DurationHours = durationHours;
+            DurationMinutes = durationMinutes;
+            Price = price;
+        }
+    }
+}

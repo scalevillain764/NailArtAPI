bbf5421cace6547df5c32e669f0167e9086f16c6
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-09-15 08:35:56 +0300

    feat: add AppDbContext

---
+++ NailArt/Infrastructure/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Booking> bookings { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<Service> services { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Booking>()
+                .HasOne(b => b.User)
+                .WithMany(u => u.bookings);
+
+            builder.Entity<Booking>()
+               .HasOne(b => b.Service)
+               .WithMany(s => s.bookings);
+        }
+    }
+}

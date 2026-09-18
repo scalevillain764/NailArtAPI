511a3c6207d83d2084021eaab63d49af1431ef6c
Max <kvyatkovskymaxim7@gmail.com>
2026-09-18 20:53:03 +0300

    feat: load TelegramBot; add IBotHandler interface & telegram hosted service

---
+++ NailArt/TelegramBot/Interfaces/IBotHandler.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot;
+namespace TelegramBot.Interfaces
+{
+    public interface IBotHandler
+    {
+        bool CanHandle(Update update);
+    
+        Task HandleAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken);
+    }
+}
---
+++ NailArt/TelegramBot/Services/TelegramHostedService.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+namespace TelgramBot.Services
+{
+    public class TelegramBotHostedService : BackgroundService
+    {
+
+    }
+}
